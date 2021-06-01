deed79df0c7823f35810563a24402e92b8a6d020
gilbertsanl <71920942+gilbertsanl@users.noreply.github.com>
2021-06-01 16:44:46 +0700

    Masuk ke Login dan Beranda
    Co-Authored-By: Imanuel David Pratista <46641452+Dante1724@users.noreply.github.com>
    Co-Authored-By: Paul R Y Pasaribu <45089304+paulronaldo@users.noreply.github.com>

--- src/test/java/OnBoardingEventTesting/Main.java
+++ src/test/java/OnBoardingEventTesting/Main.java
@@ -1,10 +1,45 @@
 package OnBoardingEventTesting;
 
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    HashMap<String, Object> map = new HashMap<String, Object>();
+    map.put("profile.default_content_setting_values.notifications", 2);
+    ChromeOptions options = new ChromeOptions();
+    options.setExperimentalOption("prefs", map);
+    WebDriver driver = new ChromeDriver(options);
+    //driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    
+    //masuk ke halaman login
+    driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
+    
+    //login lalu masuk ke dashboard
+    WebElement element = driver.findElement(By.xpath("//*[@id = 'txtUsername']"));
+    element.clear();
+    element.sendKeys("admin");
+    WebElement element2 = driver.findElement(By.xpath("//*[@id = 'txtPassword']"));
+    element2.clear();
+    element2.sendKeys("admin123");
+    element2.submit();
+    
+    //masuk ke halaman on boarding events
+    driver.get("https://orangehrm-demo-6x.orangehrmlive.com/client/#/noncore/onboarding/viewJobs/reset/1");
+    
+    
+    
+    
+  }
 
 }
